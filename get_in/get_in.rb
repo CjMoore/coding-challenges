e19c4e58d34f7c1c6a5f87cb1834e2edbce7b75b
Charlotte Moore <mooc123@gmail.com>
2017-06-20 14:55:00 -0600

    changed strategies, now passes

--- get_in/get_in.rb
+++ get_in/get_in.rb
@@ -1,12 +1,10 @@
 require 'pry'
 
 def get_in(data, keys)
-  data.keys.map do |key|
-    new_data = data[keys[0]]
-    if new_data.kind_of?(Hash)
-      get_in(new_data, keys[1..-1])
-    else
-      return new_data
-    end
+  final_data = nil
+  keys.each do |key|
+    data = data[key]
+    final_data = data
   end
+  final_data
 end
