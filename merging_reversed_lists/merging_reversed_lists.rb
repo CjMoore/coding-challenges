1bb94c475eab8807e7980486fe67e780e5b88e03
Charlotte Moore <mooc123@gmail.com>
2017-06-20 15:33:48 -0600

    took out comments

--- merging_reversed_lists/merging_reversed_lists.rb
+++ merging_reversed_lists/merging_reversed_lists.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 def merging_reversed_lists(final_arr=[], arr1, arr2)
-
   unless arr1[0].nil? || arr2[0].nil?
     if arr1[0] > arr2[0]
       final_arr.unshift(arr1[0])
@@ -25,20 +24,4 @@ def merging_reversed_lists(final_arr=[], arr1, arr2)
       final_arr
     end
   end
-  # arr1.each_with_index do |element, index|
-  #   unless arr2[index].nil?
-  #     # binding.pry
-  #     if arr2[index] > element
-  #       final_arr.unshift(element)
-  #       final_arr.unshift(arr2[index])
-  #     else
-  #       final_arr.unshift(element)
-  #       final_arr.unshift(arr2[index])
-  #     end
-  #   else
-  #     final_arr.unshift(element)
-  #   end
-  # end
-
-  final_arr
 end
